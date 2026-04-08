b42ffcd9becfc297492dd64c16b2f2f6e4d44e84
BigOrangeQWQ <2284086963@qq.com>
2026-04-08 15:54:08 +0800

    feat: Add AdapterManager and get_adapter_manager to module exports

--- src/omg_cli/config/__init__.py
+++ src/omg_cli/config/__init__.py
@@ -1,5 +1,6 @@
 """Configuration management for omg-cli."""
 
+from src.omg_cli.config.adapter_manager import AdapterManager, get_adapter_manager
 from src.omg_cli.config.constants import (
     DEFAULT_CONFIG_DIR,
     DEFAULT_CONFIG_FILE,
@@ -18,6 +19,7 @@ __all__ = [
     "DEFAULT_HISTORY_FILE",
     "DEFAULT_MODELS_FILE",
     "MAX_HISTORY_SIZE",
+    "AdapterManager",
     "ConfigManager",
     "InputHistory",
     "ModelConfig",
@@ -25,5 +27,6 @@ __all__ = [
     "SessionMetadata",
     "SessionStorage",
     "UserConfig",
+    "get_adapter_manager",
     "get_config_manager",
 ]
