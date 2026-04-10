1b6449b6e9db067b778c800fcf7488e7e09aa261
BigOrangeQWQ <2284086963@qq.com>
2026-04-10 22:36:59 +0800

    chore: 更新 __all__ 导出列表

--- omg_cli/types/__init__.py
+++ omg_cli/types/__init__.py
@@ -1,4 +1,15 @@
+from omg_cli.types.channel import Channel, Role, Thread, ThreadStatus
 from omg_cli.types.command import CommandHandler, CommandProtocol, CommandRegistry, MetaCommand
 from omg_cli.types.usage import TokenUsage
 
-__all__ = ["CommandHandler", "CommandProtocol", "CommandRegistry", "MetaCommand", "TokenUsage"]
+__all__ = [
+    "Channel",
+    "CommandHandler",
+    "CommandProtocol",
+    "CommandRegistry",
+    "MetaCommand",
+    "Role",
+    "Thread",
+    "ThreadStatus",
+    "TokenUsage",
+]
